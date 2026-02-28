dbd20af063a459ba87fce8fe929b6c76f072560d
Daniil Salnikov <daniil.salnikov.06@mail.ru>
2026-02-28 20:28:54 +0300

    Add HashMap vs TreeMap performance comparison benchmark

---
+++ Task24/Task24/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Task24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedPut = new ZedGraph.ZedGraphControl();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.zedGet = new ZedGraph.ZedGraphControl();
+            this.zedRemove = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedPut
+            // 
+            this.zedPut.Location = new System.Drawing.Point(53, 33);
+            this.zedPut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedPut.Name = "zedPut";
+            this.zedPut.ScrollGrace = 0D;
+            this.zedPut.ScrollMaxX = 0D;
+            this.zedPut.ScrollMaxY = 0D;
+            this.zedPut.ScrollMaxY2 = 0D;
+            this.zedPut.ScrollMinX = 0D;
+            this.zedPut.ScrollMinY = 0D;
+            this.zedPut.ScrollMinY2 = 0D;
+            this.zedPut.Size = new System.Drawing.Size(427, 532);
+            this.zedPut.TabIndex = 0;
+            this.zedPut.UseExtendedPrintDialog = true;
+            this.zedPut.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(686, 585);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(183, 32);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(896, 593);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(57, 16);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Process";
+            // 
+            // zedGet
+            // 
+            this.zedGet.Location = new System.Drawing.Point(549, 33);
+            this.zedGet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGet.Name = "zedGet";
+            this.zedGet.ScrollGrace = 0D;
+            this.zedGet.ScrollMaxX = 0D;
+            this.zedGet.ScrollMaxY = 0D;
+            this.zedGet.ScrollMaxY2 = 0D;
+            this.zedGet.ScrollMinX = 0D;
+            this.zedGet.ScrollMinY = 0D;
+            this.zedGet.ScrollMinY2 = 0D;
+            this.zedGet.Size = new System.Drawing.Size(438, 531);
+            this.zedGet.TabIndex = 3;
+            this.zedGet.UseExtendedPrintDialog = true;
+            // 
+            // zedRemove
+            // 
+            this.zedRemove.Location = new System.Drawing.Point(1043, 33);
+            this.zedRemove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedRemove.Name = "zedRemove";
+            this.zedRemove.ScrollGrace = 0D;
+            this.zedRemove.ScrollMaxX = 0D;
+            this.zedRemove.ScrollMaxY = 0D;
+            this.zedRemove.ScrollMaxY2 = 0D;
+            this.zedRemove.ScrollMinX = 0D;
+            this.zedRemove.ScrollMinY = 0D;
+            this.zedRemove.ScrollMinY2 = 0D;
+            this.zedRemove.Size = new System.Drawing.Size(427, 532);
+            this.zedRemove.TabIndex = 4;
+            this.zedRemove.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1523, 681);
+            this.Controls.Add(this.zedRemove);
+            this.Controls.Add(this.zedGet);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.zedPut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedPut;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private ZedGraph.ZedGraphControl zedGet;
+        private ZedGraph.ZedGraphControl zedRemove;
+    }
+}
+
---
+++ Task24/Task24/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using ZedGraph;
+using System.Collections.Generic;
+
+namespace Task24
+{
+    public partial class Form1 : Form
+    {
+        int[] sizes = { 1000, 10000, 100000 };
+        int runs = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Running benchmark...";
+            Application.DoEvents();
+
+            var putHash = BenchmarkPutHash();
+            var putTree = BenchmarkPutTree();
+
+            var getHash = BenchmarkGetHash();
+            var getTree = BenchmarkGetTree();
+
+            var removeHash = BenchmarkRemoveHash();
+            var removeTree = BenchmarkRemoveTree();
+
+            DrawGraph(zedPut, "PUT", putHash, putTree);
+            DrawGraph(zedGet, "GET", getHash, getTree);
+            DrawGraph(zedRemove, "REMOVE", removeHash, removeTree);
+
+            lblStatus.Text = "Done!";
+        }
+
+        // PUT
+
+        double[] BenchmarkPutHash()
+        {
+            return BenchmarkPut((n) => new MyHashMap<int, int>(), true);
+        }
+
+        double[] BenchmarkPutTree()
+        {
+            return BenchmarkPut((n) => new MyTreeMap<int, int>(), false);
+        }
+
+        double[] BenchmarkPut(Func<int, object> creator, bool isHash)
+        {
+            double[] result = new double[sizes.Length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                double total = 0;
+
+                for (int r = 0; r < runs; r++)
+                {
+                    var sw = Stopwatch.StartNew();
+
+                    if (isHash)
+                    {
+                        var map = new MyHashMap<int, int>();
+                        for (int j = 0; j < sizes[i]; j++)
+                            map.Put(rnd.Next(), j);
+                    }
+                    else
+                    {
+                        var map = new MyTreeMap<int, int>();
+                        for (int j = 0; j < sizes[i]; j++)
+                            map.Put(rnd.Next(), j);
+                    }
+
+                    sw.Stop();
+                    total += sw.Elapsed.TotalMilliseconds;
+                }
+
+                result[i] = total / runs;
+            }
+
+            return result;
+        }
+
+        // GET
+
+        double[] BenchmarkGetHash()
+        {
+            return BenchmarkGet(true);
+        }
+
+        double[] BenchmarkGetTree()
+        {
+            return BenchmarkGet(false);
+        }
+
+        double[] BenchmarkGet(bool isHash)
+        {
+            double[] result = new double[sizes.Length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                double total = 0;
+
+                for (int r = 0; r < runs; r++)
+                {
+                    if (isHash)
+                    {
+                        var map = new MyHashMap<int, int>();
+                        List<int> keys = new List<int>();
+
+                        for (int j = 0; j < sizes[i]; j++)
+                        {
+                            int key = rnd.Next();
+                            keys.Add(key);
+                            map.Put(key, j);
+                        }
+
+                        var sw = Stopwatch.StartNew();
+                        foreach (var key in keys)
+                            map.Get(key);
+                        sw.Stop();
+
+                        total += sw.Elapsed.TotalMilliseconds;
+                    }
+                    else
+                    {
+                        var map = new MyTreeMap<int, int>();
+                        List<int> keys = new List<int>();
+
+                        for (int j = 0; j < sizes[i]; j++)
+                        {
+                            int key = rnd.Next();
+                            keys.Add(key);
+                            map.Put(key, j);
+                        }
+
+                        var sw = Stopwatch.StartNew();
+                        foreach (var key in keys)
+                            map.Get(key);
+                        sw.Stop();
+
+                        total += sw.Elapsed.TotalMilliseconds;
+                    }
+                }
+
+                result[i] = total / runs;
+            }
+
+            return result;
+        }
+
+        // REMOVE
+
+        double[] BenchmarkRemoveHash()
+        {
+            return BenchmarkRemove(true);
+        }
+
+        double[] BenchmarkRemoveTree()
+        {
+            return BenchmarkRemove(false);
+        }
+
+        double[] BenchmarkRemove(bool isHash)
+        {
+            double[] result = new double[sizes.Length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                double total = 0;
+
+                for (int r = 0; r < runs; r++)
+                {
+                    List<int> keys = new List<int>();
+
+                    if (isHash)
+                    {
+                        var map = new MyHashMap<int, int>();
+                        for (int j = 0; j < sizes[i]; j++)
+                        {
+                            int key = rnd.Next();
+                            keys.Add(key);
+                            map.Put(key, j);
+                        }
+
+                        var sw = Stopwatch.StartNew();
+                        foreach (var key in keys)
+                            map.Remove(key);
+                        sw.Stop();
+
+                        total += sw.Elapsed.TotalMilliseconds;
+                    }
+                    else
+                    {
+                        var map = new MyTreeMap<int, int>();
+                        for (int j = 0; j < sizes[i]; j++)
+                        {
+                            int key = rnd.Next();
+                            keys.Add(key);
+                            map.Put(key, j);
+                        }
+
+                        var sw = Stopwatch.StartNew();
+                        foreach (var key in keys)
+                            map.Remove(key);
+                        sw.Stop();
+
+                        total += sw.Elapsed.TotalMilliseconds;
+                    }
+                }
+
+                result[i] = total / runs;
+            }
+
+            return result;
+        }
+
+        // GRAPH
+
+        void DrawGraph(ZedGraphControl control, string title, double[] hashData, double[] treeData)
+        {
+            GraphPane pane = control.GraphPane;
+
+            pane.Title.Text = title + " Comparison";
+            pane.XAxis.Title.Text = "Size";
+            pane.YAxis.Title.Text = "Time (ms)";
+
+            pane.CurveList.Clear();
+
+            PointPairList hashList = new PointPairList();
+            PointPairList treeList = new PointPairList();
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                hashList.Add(sizes[i], hashData[i]);
+                treeList.Add(sizes[i], treeData[i]);
+            }
+
+            pane.AddCurve("HashMap", hashList, System.Drawing.Color.Blue);
+            pane.AddCurve("TreeMap", treeList, System.Drawing.Color.Red);
+
+            control.AxisChange();
+            control.Invalidate();
+        }
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

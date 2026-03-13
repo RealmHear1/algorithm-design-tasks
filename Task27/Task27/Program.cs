c4f5c99f2a2c341eb97f1e207530d4c3ad605647
Daniil Salnikov <daniil.salnikov.06@mail.ru>
2026-03-13 20:21:40 +0300

    I made a task to read from a file into a set (unique words)

---
+++ Task27/Task27/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyHashSet<string> set = new MyHashSet<string>();
+
+        if (!File.Exists("input.txt"))
+        {
+            Console.WriteLine("File input.txt not found");
+            return;
+        }
+
+        string text = File.ReadAllText("input.txt");
+
+        Regex regex = new Regex("[A-Za-z]+");
+
+        MatchCollection matches = regex.Matches(text);
+
+        for (int i = 0; i < matches.Count; i++)
+        {
+            string word = matches[i].Value.ToLower();
+
+            set.add(word);
+        }
+
+        Console.WriteLine("Unique words:");
+
+        string[] words = set.toArray(new string[0]);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
